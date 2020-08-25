2253094e12256dc3b5b059e7def8069e95233e62
delxcign <57690820+delxcign@users.noreply.github.com>
2020-08-25 19:47:16 +0700

    commit

--- app/src/main/java/id/sch/smktelkom/appprofile/MainActivity.kt
+++ app/src/main/java/id/sch/smktelkom/appprofile/MainActivity.kt
@@ -1,9 +1,12 @@
 package id.sch.smktelkom.appprofile
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,59 +20,75 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        edtName = findViewById(R.id.edtName)
-        spinnerGender = findViewById(R.id.spinnerGender)
-        edtEmail = findViewById(R.id.edtEmail)
-        edtTelp = findViewById(R.id.edtTelp)
+        edtName = findViewById(R.id.edtNama)
+        spinnerGender = findViewById(R.id.spinnerGenderr)
+        edtEmail = findViewById(R.id.edtEmaill)
+        edtTelp = findViewById(R.id.edtTelpon)
         edtAlamat = findViewById(R.id.edtAddress)
         btnSave.setOnClickListener { validasiInput() }
 
+        setDataSpinnerGender()
+
+    }
+
+    private fun setDataSpinnerGender() {
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.gender_list,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerGenderr.adapter = adapter
     }
 
     private fun validasiInput() {
-        val namaInput = edtName.text.toString()
-        val emailInput = edtEmail.text.toString()
-        val telpInput = edtTelp.text.toString()
+        val namaInput = edtNama.text.toString()
+        val emailInput = edtEmaill.text.toString()
+        val telpInput = edtTelpon.text.toString()
         val alamatInput = edtAddress.text.toString()
-        val genderInput = spinnerGender.selectedItem.toString()
+        val genderInput = spinnerGenderr.selectedItem.toString()
+
         when {
-            namaInput.isEmpty() -> edtName.error = "Nama tidak boleh kosong"
-            genderInput.equals("Pilih kelamin") -> tampilToast("Kelamin harus dipilih")
-            emailInput.isEmpty() -> edtEmail.error = "Email tidak bolehkosong"
-            telpInput.isEmpty() -> edtTelp.error = "Telp tidak boleh kosong"
-            alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak bolehkosong"
+            namaInput.isEmpty() -> edtNama.error = "Nama tidak boleh kosong"
+            genderInput.equals("Pilih kelamin") -> Toast.makeText(
+                this,
+                "Kelamin harus dipilih",
+                Toast.LENGTH_SHORT
+            )
+            emailInput.isEmpty() -> edtEmaill.error = "Email tidak boleh kosong"
+            telpInput.isEmpty() -> edtTelpon.error = "Telp tidak boleh kosong"
+            alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak boleh kosong"
             else -> {
-                tampilToast("Navigasi ke halaman profil diri")
+                Toast.makeText(this, "Navigasi ke halaman profil diri", Toast.LENGTH_SHORT).show()
                 navigasiKeProfilDiri()
 
             }
-        }
-
-        fun setDataSpinnerGender() {
-            val adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.gender_list, android.R.layout.simple_spinner_item
-            )
-            adapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item
-            )
-            spinnerGender.adapter = adapter
-
         }
 
     }
 
     private fun navigasiKeProfilDiri() {
         val intent = Intent(this, profile::class.java)
-
         val bundle = Bundle()
+
+        val namaInput = edtNama.text.toString()
+        val emailInput = edtEmaill.text.toString()
+        val telpInput = edtTelpon.text.toString()
+        val alamatInput = edtAddress.text.toString()
+        val genderInput = spinnerGenderr.selectedItem.toString()
+
+
         bundle.putString("nama", namaInput)
         bundle.putString("gender", genderInput)
         bundle.putString("email", emailInput)
         bundle.putString("telp", telpInput)
         bundle.putString("alamat", alamatInput)
+
         intent.putExtras(bundle)
         startActivity(intent)
     }
+
+
 }
+
+
--- app/src/main/java/id/sch/smktelkom/appprofile/profile.kt
+++ app/src/main/java/id/sch/smktelkom/appprofile/profile.kt
@@ -1,27 +1,32 @@
 package id.sch.smktelkom.appprofile
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        ambilData()
     }
 
     private fun ambilData() {
         val bundle = intent.extras
+
         val nama = bundle?.getString("nama")
         val gender = bundle?.getString("gender")
         val email = bundle?.getString("email")
         val telp = bundle?.getString("telp")
         val alamat = bundle?.getString("alamat")
+
         txtName.text = nama
         txtGender.text = gender
         txtEmail.text = email
         txtTelp.text = telp
         txtAddress.text = alamat
+
+
     }
 }
